e035cc9a0fab37a2fc254bfbc6f26f6c0cfdf195
Joel Stüdle <joel.stuedle@gmail.com>
2020-03-23 15:41:25 +0100

    processing view (loaders), replace default autoptimize critical css with critical-css.io tab

--- .build/assets/scripts/admin/index.js
+++ .build/assets/scripts/admin/index.js
@@ -9,16 +9,40 @@
 		const $wpbody = $('body #wpcontent');
 		$wpbody.append('<div class="aoccssio-loader"></div>');
 		const $loader = $wpbody.find('.aoccssio-loader');
+		const $wp_spinner = $('<img src="./images/spinner.gif" />');
+		const $wp_loading = $('<img src="./images/loading.gif" style="margin-right: 5px;" />');
+
+		const genLoading = function(loading, $e) {
+			const $url_input = $e.find('[name="aoccssio_url"]');
+			const $trigger_generate = $e.find('.aoccssio-generate__regenerate');
+			const $trigger_delete = $e.find('.aoccssio-generate__delete');
+			const $trigger_controls = $e.find('.aoccssio-generate__controls');
+			const $trigger_status = $e.find('.aoccssio-generate__status');
+			
+			if (loading) {
+				$url_input.attr('disabled', true);
+				$trigger_delete.add($trigger_generate).add($trigger_status).hide();
+				$trigger_controls.append($wp_spinner);
+				$wp_loading.insertBefore($trigger_status);
+			} else {
+				$url_input.attr('disabled', false);
+				$trigger_delete.add($trigger_generate).add($trigger_status).show();
+				$wp_spinner.add($wp_loading).remove();
+			}
+		}
 
 		$elements.each(function () {
 			const $e = $(this);
 			const $url_input = $e.find('[name="aoccssio_url"]');
 			const $trigger_generate = $e.find('.aoccssio-generate__regenerate');
 			const $trigger_delete = $e.find('.aoccssio-generate__delete');
-			console.log($trigger_generate.length);
+			// console.log($trigger_generate.length);
 
 			$trigger_generate.on('click', function () {
+
+				genLoading(true, $e);
 				const url = $url_input.val();
+
 				if (!valid_url(url)) {
 					$url_input.addClass('aoccssio-generate__input--error-pop');
 					setTimeout(function () {
@@ -42,7 +66,7 @@
 					data: val
 				}).done(function (data) {
 
-					$loader.fadeOut();
+					genLoading(false, $e);
 
 					if (data['type'] === null || data['type'] !== 'success') {
 
@@ -66,11 +90,13 @@
 						$e.find('.is_generated').text(data['add']['datetime']);
 						$e.removeClass('aoccssio-generate--nofile');
 					}
+
 				});
 			});
 
 			$trigger_delete.on('click', function () {
 
+				genLoading(true, $e);
 				let vals = [];
 				vals.push('action=' + $e.find('input[name=criticalapi_action_delete]').val());
 				vals.push('critical_key=' + $e.find('input[name=criticalapi_key]').val());
@@ -86,6 +112,8 @@
 					dataType: 'json',
 					data: val
 				}).done(function (data) {
+
+					genLoading(false, $e);
 
 					if (data['type'] === null || data['type'] !== 'success') {
 
